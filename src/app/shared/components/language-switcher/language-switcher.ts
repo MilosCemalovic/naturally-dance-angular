bddee97f0339dd63505da2d066b3c2595975771d
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-27 13:26:24 +0200

    Language-switcher component setup

---
+++ src/app/shared/components/language-switcher/language-switcher.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core'
+import { TranslocoService } from '@jsverse/transloco'
+import { ButtonModule } from 'primeng/button'
+import { LanguageServiceTs } from '../../../core/services/language.service.ts'
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [ButtonModule],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.scss'
+})
+export class LanguageSwitcher
+{
+  constructor(public translocoService: TranslocoService, private languageService: LanguageServiceTs) {}
+
+  switchLanguage ()
+  {
+    const newLang=this.translocoService.getActiveLang()==='en'? 'sr':'en'
+    this.languageService.switchLanguage(newLang)
+  }
+
+}
