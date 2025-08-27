c2a2785a3f6086ff6024817b139abd6a047e751c
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-27 16:44:30 +0200

    Events component setup

---
+++ src/app/features/events/events.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+import { TranslocoModule } from '@jsverse/transloco'
+import { CardModule } from 'primeng/card'
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  imports: [TranslocoModule, CardModule],
+  templateUrl: './events.html',
+  styleUrl: './events.scss'
+})
+export class Events
+{
+
+}
