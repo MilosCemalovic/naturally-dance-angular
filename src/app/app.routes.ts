5f21b1f32e794298377a07459e85d7311979c5aa
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-27 16:46:20 +0200

    Routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes=[
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./features/about/about.routes').then(m => m.ABOUT_ROUTES)
+  },
+  {
+    path: 'events',
+    loadChildren: () => import('./features/events/events.routes').then(m => m.EVENTS_ROUTES)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./features/contact/contact').then(m => m.Contact)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
