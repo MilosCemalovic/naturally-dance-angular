fbdb472a76702806ceebc860fa48c9db84f0b66d
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-27 13:30:58 +0200

    Header component setup

---
+++ src/app/shared/components/header/header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+import { TranslocoModule } from '@jsverse/transloco'
+import { LanguageSwitcher } from '../language-switcher/language-switcher'
+import { ButtonModule } from 'primeng/button'
+
+@Component({
+  selector: 'app-header',
+  imports: [TranslocoModule, LanguageSwitcher, ButtonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class Header
+{
+
+}
