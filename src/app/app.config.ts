45c616796940f2a4c1159f29149f5fa175883325
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-27 12:56:33 +0200

    Translation files setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import
+{
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+  isDevMode,
+} from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideHttpClient } from '@angular/common/http'
+import { provideTransloco } from '@jsverse/transloco'
+import { TranslocoHttpLoader } from './transloco-loader'
+import { routes } from './app.routes'
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { TranslocoHttpLoader } from './transloco-loader';
-import { provideTransloco } from '@jsverse/transloco';
-
-export const appConfig: ApplicationConfig = {
+export const appConfig: ApplicationConfig={
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideHttpClient(), provideTransloco({
-        config: { 
-          availableLangs: ['en', 'sr'],
-          defaultLang: 'en',
-          // Remove this option if your application doesn't support changing language in runtime.
-          reRenderOnLangChange: true,
-          prodMode: !isDevMode(),
-        },
-        loader: TranslocoHttpLoader
-      })
-  ]
-};
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'sr'],
+        defaultLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
+}
