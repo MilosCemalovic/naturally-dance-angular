d0aea0041e074421688573ac296e5f5b7e7c64c5
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-29 12:29:06 +0200

    App component refactor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Navigation } from './shared/components/navigation/navigation'
+import { Footer } from './shared/components/footer/footer'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('naturally-dance');
+export class App
+{
+  protected readonly title=signal('naturally-dance');
 }
