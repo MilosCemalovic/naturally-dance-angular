d91fdfdfa4e2d9dcceba48afde5bdd539291e557
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-29 15:42:03 +0200

    Language service refactor

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -8,10 +8,13 @@ export class LanguageService
 {
   constructor(private translocoService: TranslocoService) {}
 
-  switchLanguage (language: string): void
+  switchLanguage (language: string)
   {
     this.translocoService.setActiveLang(language)
     localStorage.setItem('preferredLanguage', language)
+
+    // Reload the page to ensure all content is translated
+    window.location.reload()
   }
 
   getCurrentLanguage (): string
@@ -19,14 +22,10 @@ export class LanguageService
     return this.translocoService.getActiveLang()
   }
 
-  initializeLanguage (): void
+  initializeLanguage ()
   {
     const savedLanguage=localStorage.getItem('preferredLanguage')
-    const defaultLanguage='en'
-    // const languageToSet = savedLanguage || defaultLanguage
-
     if (savedLanguage&&this.translocoService.isLang(savedLanguage)) {
-      // this.translocoService.setActiveLang(languageToSet)
       this.translocoService.setActiveLang(savedLanguage)
     }
   }
