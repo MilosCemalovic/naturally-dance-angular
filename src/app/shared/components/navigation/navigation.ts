71ae0f50b66d995a0dd9323f50cc4f90e3df6236
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-29 12:27:18 +0200

    Navigation component setup

---
+++ src/app/shared/components/navigation/navigation.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core'
+import { MenubarModule } from 'primeng/menubar'
+import { TranslocoModule } from '@jsverse/transloco'
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [MenubarModule, TranslocoModule],
+  templateUrl: './navigation.html',
+  styleUrls: ['./navigation.scss']
+})
+export class Navigation
+{
+  items=[
+    { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
+    { label: 'About Us', icon: 'pi pi-info-circle', routerLink: '/about' },
+    { label: 'Events', icon: 'pi pi-calendar', routerLink: '/events' },
+    { label: 'Contact', icon: 'pi pi-envelope', routerLink: '/contact' }
+  ];
+}
