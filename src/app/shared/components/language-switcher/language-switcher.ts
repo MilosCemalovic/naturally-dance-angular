3fab9702ac56b2f8a9eb55694d32e659a661d2bb
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-08-30 16:47:22 +0200

    Implementing country flags/icons and dropdown button inside navigation

--- src/app/shared/components/language-switcher/language-switcher.ts
+++ src/app/shared/components/language-switcher/language-switcher.ts
@@ -1,20 +1,49 @@
 import { Component } from '@angular/core'
 import { TranslocoService } from '@jsverse/transloco'
-import { ButtonModule } from 'primeng/button'
+import { DropdownModule } from 'primeng/dropdown'
 import { LanguageService } from '../../../core/services/language.service'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+
+interface LanguageOption {
+  label: string
+  value: string
+  countryCode: string
+}
 
 @Component({
   selector: 'app-language-switcher',
-  imports: [ButtonModule],
+  standalone: true,
+  imports: [DropdownModule, FormsModule, CommonModule],
   templateUrl: './language-switcher.html',
   styleUrl: './language-switcher.scss'
 })
 export class LanguageSwitcher {
-  constructor(public translocoService: TranslocoService, private languageService: LanguageService) {}
+  languages: LanguageOption[] = [
+    {
+      label: ' English',
+      value: 'en',
+      countryCode: 'gb',
+    },
+    {
+      label: ' Srpski',
+      value: 'sr',
+      countryCode: 'rs',
+    }
+  ];
+
+  selectedLanguage: LanguageOption
+
+  constructor(public translocoService: TranslocoService, private languageService: LanguageService) {
+    // Set initial selected language based on current active language
+    const currentLang = this.translocoService.getActiveLang()
+    this.selectedLanguage = this.languages.find(lang => lang.value === currentLang) || this.languages[0]
+  }
 
   switchLanguage () {
-    const newLang = this.translocoService.getActiveLang() === 'en' ? 'sr' : 'en'
-    this.languageService.switchLanguage(newLang)
+    if (this.selectedLanguage) {
+      this.languageService.switchLanguage(this.selectedLanguage.value)
+    }
   }
 
 }
