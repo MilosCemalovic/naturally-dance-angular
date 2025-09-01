4b2f72747ef9369cc7e9f3b4303352756e758085
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-01 16:46:17 +0200

    Added clickable links and animation

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { TranslocoModule } from '@jsverse/transloco'
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, CommonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrls: ['./footer.scss']
 })
-export class Footer {
+export class Footer implements OnInit {
+  @ViewChild('footerElement') footerElement!: ElementRef
 
+  ngOnInit () {
+    // Initial check in case footer is already in view
+    setTimeout(() => this.checkScroll(), 100)
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll () {
+    this.checkScroll()
+  }
+
+  checkScroll () {
+    if (!this.footerElement) return
+
+    const element = this.footerElement.nativeElement
+    const elementPosition = element.getBoundingClientRect().top
+    const screenPosition = window.innerHeight / 1.3 // Trigger when ⅓ from bottom
+
+    if (elementPosition < screenPosition) {
+      element.classList.add('animate')
+    }
+  }
 }
